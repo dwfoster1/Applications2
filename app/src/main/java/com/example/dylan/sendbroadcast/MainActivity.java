801d6a9af49af261826d107132809cf292db881c
Dylan <dwfoster1@catamount.wcu.edu>
2015-10-23 15:09:30 -0400

    SendBroadcast now sends broadcasts to any app that is listening for them

---
+++ app/src/main/java/com/example/dylan/sendbroadcast/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.dylan.sendbroadcast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendOutBroadcast(View view) {
+        //just going to broadcast a message to anyone who is listening
+        Intent i = new Intent();
+        //only want to listen from this intent "This Broadcast"
+        i.setAction("com.example.dylan.sendbroadcast");
+        //to make sure this intent broadcasts with all android we have to have this
+        i.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendBroadcast(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
